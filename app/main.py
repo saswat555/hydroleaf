9eb80dac55aa6e063cf3f2d872e0b6eade78a2e6
saswat555 <sapa19is@cmrit.ac.in>
2025-05-05 14:17:29 +0530

    fixes

--- app/main.py
+++ app/main.py
@@ -84,8 +84,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
